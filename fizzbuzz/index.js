d8fd886e913b4b0a87005a51204e4f4335e1b2ee
Sam Walker <jack.nasal@gmail.com>
2019-07-11 16:30:49 -0400

    finished fizzbuzzer

--- fizzbuzz/index.js
+++ fizzbuzz/index.js
@@ -1,29 +1,49 @@
 'use strict';
 
+function decoder(num) {
+  if (num%15===0) {
+    return `
+      <div class="fizz-buzz-item fizzbuzz">
+        <span>fizzbuzz</span>
+      </div>
+      `;
+  }
+  if (num%5===0) {
+    return `
+      <div class="fizz-buzz-item fizz">
+        <span>fizz</span>
+      </div>
+    `;
+  }
+  if (num%3===0) {
+    return `
+      <div class="fizz-buzz-item buzz">
+        <span>buzz</span>
+      </div>
+    `;
+  }
+  return `
+    <div class="fizz-buzz-item">
+      <span>${num}</span>
+    </div>
+  `;
+}
+
+function writer(num) {
+  for (let i=1; i<=num; i++) {
+    $('.js-results').append(decoder(i));
+  }
+}
+
 function start() {
   $('#number-chooser').submit(function() {
     event.preventDefault();
-    console.log('submit');
-    let numChoice = $('#number-choice').val();
-    console.log(numChoice);
-    // const output = function() {
-    //   for (let i = 0; i < )
-    // }
-    // $('.js-results').append(output);
+    let numChoice = $('#number-choice');
+    writer(numChoice.val());
+    numChoice.val('');
+    
   });
   console.log('page loaded');
 }
 
 $(start);
-
-{/* <div class="fizz-buzz-item fizz">
-  <span>fizz</span>
-</div>
-
-<div class="fizz-buzz-item buzz">
-  <span>buzz</span>
-</div>
-
-<div class="fizz-buzz-item fizzbuzz">
-  <span>fizzbuzz</span>
-</div> */}
